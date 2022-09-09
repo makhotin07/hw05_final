417dd8e81cd85a4d8891dadda151a850e34f1d95
Nikita Makhotin <makhotin.07@gmail.com>
2022-09-09 17:18:11 +0300

    1 rew 1 pep

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -6,7 +6,7 @@ from django.contrib.auth import get_user_model
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import Client, TestCase, override_settings
 from django.urls import reverse
-from posts.models import Group, Post, Comment
+from posts.models import Group, Post
 
 User = get_user_model()
 TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
